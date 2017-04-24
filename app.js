4836eb46abf729dd7203b5e978afd58b29b19404
Brittany <Briy_@Brittanys-MacBook-Air.local>
2017-04-24 14:43:16 -0700

    created my own express server

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require('express')
 const app = express()
 
 app.get('/', function(req, res) {
-res.send('This is my express server')
+  res.send('This is how we do it!!')
 })
 
 app.listen(3000, function() {
-console.log('This is how we do it, on port 3000!')
+  console.log('You actually did it!!!! You accesed port 3000')
 })
