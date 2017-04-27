488d301c9ee63ae53584bc1e84f4325a90e605f6
Brittany <heyjessie510@gmail.com>
2017-04-26 17:06:23 -0700

    added viewport to pug file

--- app.js
+++ app.js
@@ -5,22 +5,21 @@ const app = express()
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
-});
+})
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
-});
-
-// app.get('/', function(req, res) {
-//   res.send('This is how we do i')
-// })
+})
 
 app.get('/', function (req, res) {
   res.render('index', { title: 'Hey you there', message: 'I see you over there!' })
 })
 
 app.set('view engine', 'pug')
+
+//allows access to files in public folder
+app.use(express.static('public'))
 
 app.listen(3000, function() {
   console.log('You actually did it!!!! You accesed port 3000')
